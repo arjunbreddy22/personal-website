6bdb0a6ece5b1bd84e456c83c4758408495ab452
cookiesupers22 <112850268+cookiesupers22@users.noreply.github.com>
2024-12-25 17:21:26 -0800

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+
+function App() {
+  return <h1>Starting website</h1>;
+}
+
+export default App;
